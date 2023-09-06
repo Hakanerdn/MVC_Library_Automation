04cc7079a0d139488c50e4b309853c601d89cd17
Hakanerdn <hasanhakanerden@gmail.com>
2023-09-07 00:00:18 +0300

    Context class created.

---
+++ Entities/Model/Context/LibraryContext.cs
@@ -0,0 +1,31 @@
+﻿using Entities.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Model.Context
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext():base()
+        {
+            
+        }
+        public DbSet<About> About { get; set; }
+        public DbSet<Book> Book { get; set; }
+        public DbSet<BookMovement> BookMovement { get; set; }
+        public DbSet<BookRecordMovement> BookRecordMovement { get; set; }
+        public DbSet<BookType> BookType { get; set; }
+        public DbSet<Communication> Communication { get; set; }
+        public DbSet<Member> Member { get; set; }
+        public DbSet<Notice> Notice { get; set; }
+        public DbSet<RoleGuide> RoleGuide { get; set; }
+        public DbSet<SafetyBook> SafetyBook { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<UserMovement> UserMovement { get; set; }
+        public DbSet<UserRole> UserRole { get; set; }
+    }
+}
