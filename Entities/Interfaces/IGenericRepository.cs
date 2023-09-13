1c0c2ec6262bf65d4478a69060038b00d3123b4c
Hakanerdn <hasanhakanerden@gmail.com>
2023-09-13 17:08:34 +0300

    Generic Interface

---
+++ Entities/Interfaces/IGenericRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Interfaces
+{
+    public interface IGenericRepository<TContext, TEntity>
+        where TContext : DbContext, new()
+        where TEntity : class, new()
+    {
+        List<TEntity> GetAll(TContext context, Expression<Func<TEntity, bool>> filter = null);
+
+        TEntity GetByFilter(TContext context, Expression<Func<TEntity, bool>> filter);
+
+        TEntity GetById(TContext context, int? id);
+
+        void InsertorUpdate(TContext context, TEntity entity);
+
+        void Delete(TContext context, TEntity entity);
+
+        void Save(TContext context);
+    }
+}
