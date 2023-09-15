703b69788c0fefc63c5e2fdbb149bb40a1616092
Hakanerdn <hasanhakanerden@gmail.com>
2023-09-15 11:04:37 +0300

    DAL
    Data Access Layer

---
+++ Entities/DAL/UserDAL.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Model;
+using Entities.Model.Context;
+using Entities.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DAL
+{
+    public class UserDAL : GenericRepository<LibraryContext, User>
+    {
+    }
+}
